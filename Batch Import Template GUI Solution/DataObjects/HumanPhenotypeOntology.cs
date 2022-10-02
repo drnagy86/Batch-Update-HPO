f9e71ab31aa532210fe19302176c84ab847e1569
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2022-10-02 15:13:09 -0500

    Add forms project

---
+++ Batch Import Template GUI Solution/DataObjects/HumanPhenotypeOntology.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class HumanPhenotypeOntology
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string GeneId { get; set; }
+        public string GeneSymbol { get; set; }
+        public string GeneDatabase { get; set; }
+        public string DiseaseID { get; set; }
+
+    }
+}
